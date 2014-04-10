c1d99b3db695f1ccc5e06d75518b60e1d782058f
Tom Seward <sewardth@gmail.com>
2014-04-10 06:40:08 -0400

    Trending page fix

--- trending.py
+++ trending.py
@@ -1,6 +1,8 @@
 
 
 import webapp2, json, sys, views, random
+from google.appengine.ext import ndb
+from google.appengine.datastore.datastore_query import Cursor
 sys.path.insert(0,'libs')
 import models
 
@@ -9,15 +11,17 @@ import models
 class TrendingHandler(views.Template):
 	def get(self):
 		musicians_states = [{'name':'Michigan', 'abbr':'MI'}, {'name':'California', 'abbr':'CA'}, {'name':'Florida', 'abbr':'FL'}]
+
 		template_values = {'musicians_states':musicians_states}
 		self.render('trending.html', template_values)
+	
 
 	def post(self):
 		# NOTE: we are posting genre, state and the cursor from a previous request or null if this is the initial one.
 		curs = Cursor(urlsafe=self.request.get('cursor'))
 		musicians, next_curs, more = models.musician.Musician.query().fetch_page(10, start_cursor=curs)
 		trending_data =[]
-		for x in mus_data:
+		for x in musicians:
 			data = x.to_dict()
 			del data['profile_pic'], data['latest_update'], data['user_key'], data['account_created'], data['DOB']
 			data['mus_key'] = x.key.urlsafe()
