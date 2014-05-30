dfda9bd80a1add5e5a79fddcdfbd3e5a669a2f72
Tom Seward <sewardth@gmail.com>
2014-05-30 19:41:07 -0400

    Major code clean up to musician.py / denormalized data model for calculations

--- musician.py
+++ musician.py
@@ -8,37 +8,33 @@ import models
 class MusicianHandler(views.Template):
 	def get(self):
 		user = self.user_check()
-		hide_follow = False
-		musician_key = ndb.Key(urlsafe = self.request.get('id'))
-		musician = musician_key.get()
-		self.response.out.write(musician.musician_stats)
-		if user:
-			if user.account_type == 'musician':
-				profile = models.musician.Musician.query_by_account(user.key)
-				if musician_key == profile.key: hide_follow = True
+		musician = ndb.Key(urlsafe = self.request.get('id')).get()
 
-		videos = models.videos.Videos.fetch_by_musician(musician_key)
-		self.response.out.write(videos[0].video_stats)
-		followers = models.following.Following.fetch_by_followed_key(musician.key)
-		likes = models.voting.Voting.fetch_winning_count([x.key for x in videos])
-		if likes != None:
-			matches = models.voting.Voting.fetch_votes_musicians([musician.key])
-			total_matches = [x.video_one for x in matches] + [x.video_two for x in matches]
-			wins = [x.voter_choice for x in likes]
-			for x in videos:
-				x.total_matchups = total_matches.count(x.key)
-				x.likes =  wins.count(x.key)
-				if x.total_matchups and x.likes != 0:
-					x.win_percent = format((float(x.likes) / x.total_matchups)*100, '.0f')
-				else:
-					x.win_percent = '0'
-				
-		if user: 
-			user_following = models.following.Following.get_by_keys(self.user_check().key, musician.key)
+		if user:
+			if musician.user_key == user.key: 
+				hide_follow = True 
+				user_following = False
+
+			else:
+				hide_follow = False
+				user_following = models.following.Following.get_by_keys(self.user_check().key, musician.key)
 		else:
 			user_following = False
+			hide_follow = True
+
+		#query all videos for this artist
+		videos = models.videos.Videos.fetch_by_musician(musician.key)
+			
+
 		
-		template_values = {'hide':hide_follow,'musician':musician, 'videos':videos, 'call_b':str(self.request.path), 'followers':len(followers), 'likes':len(likes), 'is_following':user_following}
+		template_values = {'hide':hide_follow,
+						   'musician':musician, 
+						   'videos':videos, 
+						   'call_b':str(self.request.path), 
+						   'followers':musician.musician_stats.get('followers',0), 
+						   'likes':musician.musician_stats.get('likes',0)+musician.musician_stats.get('head_to_head_wins',0), 
+						   'is_following':user_following}
+						   
 		self.render('musician.html', template_values)
     		    		                                         		
 app = webapp2.WSGIApplication([
